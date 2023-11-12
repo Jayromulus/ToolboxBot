4650d12dcf54d26add2859c65b6289b4b575af75
Jayromulus <jerbear2@gmail.com>
2023-11-12 01:45:09 -0500

    Initial setup with embed command

---
+++ deployCommands.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands');
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+};
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+	try {
+		console.log(`Started updating ${commands.length} application commands...`);
+	
+		await rest.put(Routes.applicationGuildCommands(process.env.CLIENT, process.env.GUILD), { body: [] })
+			.then(() => console.log('Successfully deleted all guild commands'))
+			.catch(console.error);
+	
+		const data = await rest.put(Routes.applicationGuildCommands(process.env.CLIENT, process.env.GUILD), { body: commands });
+		
+		console.log(`Successfully updated ${data.length} guild commands`);
+	} catch (error) {
+		console.error(error)
+	}
+})();
---
+++ index.js
@@ -0,0 +1,55 @@
+// initialize environment variables
+require('dotenv').config();
+// bring in fs and path for importing and referencing individual command files
+const fs = require('node:fs');
+const path = require('node:path');
+// import the required properties from discord.js
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const token = process.env.TOKEN;
+
+// instantiate a new client giving it certain intents, originally Guilds but trying to limit permissions
+const client = new Client({ intents: [GatewayIntentBits.GuildMessages] });
+
+// only runs once when the clientready event is fired, and logs a message in the console signaling the app is ready
+client.once(Events.ClientReady, current => {
+	console.log(`Ready! Logged in as ${current.user.tag}`);
+});
+
+// create a new collection of commands the bot will know
+client.commands = new Collection();
+
+// grab the local commands folder and only get javascript files from it
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+// loop over the files and add a command to the collection if it has the required information attached
+for (const file of commandFiles) {
+	const filePath = path.join(__dirname, 'commands/', file);
+	const command = require(filePath);
+
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command)
+	} else {
+		console.warn(`[WARNING]: the command at ${filePath} is missing a required "data" or "execute" property`);
+	}
+};
+
+// every time an interaction is created run this chunk
+client.on(Events.InteractionCreate, async interaction => {
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} found`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch(error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command', ephemeral: true });
+	}
+});
+
+// log in to discord with the token
+client.login(token);
